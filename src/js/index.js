8ef3741208b09a7a2f4da7da94d9888c96dde80f
Victor Parizio <victorparizio@hotmail.com>
2022-10-24 11:50:06 -0300

    Refatoração do campo obrigatorio no JavaScript

--- src/js/index.js
+++ src/js/index.js
@@ -1,40 +1,18 @@
-const textInput = document.querySelectorAll('input, textarea')
+const textInput = document.querySelectorAll('.campo')
 const obrigatorio = document.querySelectorAll('span')
 const enviar = document.getElementById('enviar')
 
-
 enviar.addEventListener('click', () => {
-    textInput.forEach(function (item) {
+    textInput.forEach(function (item, indice) {
         if (item.value !== '') {
-            item.classList.remove('invalido')
             item.classList.add('valido')
+            item.classList.remove('invalido')
+            obrigatorio[indice].classList.remove('obrigatorio')
+
         } else if (item.value === '') {
             item.classList.add('invalido')
             item.classList.remove('valido')
+            obrigatorio[indice].classList.add('obrigatorio')
         }
     })
-
-    if(textInput[0].classList.contains('invalido')){
-        obrigatorio[0].classList.add('obrigatorio')
-    } else {
-        obrigatorio[0].classList.remove('obrigatorio')
-    }
-
-    if(textInput[1].classList.contains('invalido')){
-        obrigatorio[1].classList.add('obrigatorio')
-    } else {
-        obrigatorio[1].classList.remove('obrigatorio')
-    }
-
-    if(textInput[2].classList.contains('invalido')){
-        obrigatorio[2].classList.add('obrigatorio')
-    } else {
-        obrigatorio[2].classList.remove('obrigatorio')
-    }
-
-    if(textInput[3].classList.contains('invalido')){
-        obrigatorio[3].classList.add('obrigatorio')
-    } else {
-        obrigatorio[3].classList.remove('obrigatorio')
-    }
 })
